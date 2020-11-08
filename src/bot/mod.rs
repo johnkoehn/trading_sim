65be06c0f8bcd91c2a892306ec203891ecc3638f
John Koehn <koehn.john95@gmail.com>
2020-11-07 21:24:01 -0600

    bot traits

--- src/bot/mod.rs
+++ src/bot/mod.rs
@@ -1,5 +1,8 @@
+pub mod traits;
+use traits::Trait;
 use std::collections::HashMap;
 use crate::asset::Asset;
+use serde_json::{Value, Map, Number};
 
 #[derive(Copy, Clone, PartialEq, Eq, Hash, Debug)]
 pub enum ActionState {
@@ -8,22 +11,28 @@ pub enum ActionState {
     Netural
 }
 
-#[derive(Debug)]
 pub struct Bot {
     pub state: ActionState,
-    pub assets: HashMap<Asset, f64>
+    pub assets: HashMap<Asset, f64>,
+    pub traits: HashMap<Trait, Value>
 }
 
 impl Bot {
     pub fn new() -> Bot {
         let mut bot = Bot {
             state: ActionState::Netural,
-            assets: HashMap::new()
+            assets: HashMap::new(),
+            traits: HashMap::new()
         };
 
         for asset in Asset::iterator() {
             bot.assets.insert(*asset, 0.0);
         }
+
+        bot.traits.insert(Trait::BuyDirection,  Value::from(5));
+        let test = Value::from(5.0);
+        let test2 = Value::from(5);
+        // test2.
 
         bot
     }
---
+++ src/bot/traits.rs
@@ -0,0 +1,23 @@
+#[derive(Copy, Clone, PartialEq, Eq, Hash, Debug)]
+pub enum Direction {
+    Up,
+    Down
+}
+
+// TODO: Remove unused traits from the bot in the simulation - i.e. bots become more scoped over time
+#[derive(Copy, Clone, PartialEq, Eq, Hash, Debug)]
+pub enum Trait {
+    NumberOfAveragingPeriods, // the periods of data points to look at to derive averagings
+    BuyDirection,
+    SellDirection,
+    MinimumBuyMomentum, // Always a positive value, it acts negative when buydirection is true (decimal)
+    MaximumBuyMomentum,
+    TrailingStopLoss,
+    StopLoss,
+    MaxHoldingPeriods,
+    MinimumHoldingPeriods,
+    MaximumHoldingPeriods
+}
+
+// NOTE: Not bots get all traits. A bot can lose any set of traits due to random mutation
+// a mutation on a essential trait casues the bot to sucide -- not pass on traits
